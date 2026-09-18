c78e679cab8d3718ee4bc59f149c87f8e34c41c4
Hugo-NF <hugonfonseca@hotmail.com>
2026-09-18 08:25:27 -0300

    Install and configure VCR for HTTP interaction testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "webmock/minitest"
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+  config.default_cassette_options = { record: :once }
+end
 
 module ActiveSupport
   class TestCase
